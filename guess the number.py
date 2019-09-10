26ce5608a52330cb8cb47a387a713c99e5ede943
trinaChaudhuri <43410438+trinaChaudhuri@users.noreply.github.com>
2019-09-10 20:15:37 +0530

    Guess the Number
    The previous code uploaded is not the correct one.

--- guess the number.py
+++ guess the number.py
@@ -1,9 +1,19 @@
 import random
-for x in range(1):
-    print(random.randrange(1,100)
- 
-
-
-
-
-    
+guessesTaken = 0
+number = random.randint(1, 20)
+while guessesTaken < 6:
+    guess = input()
+    guess = int(guess)
+    guessesTaken = guessesTaken + 1
+    if guess < number:
+        print('Your guess is too low.') # There are eight spaces in front of print.
+    if guess > number:
+        print('Your guess is too high.')
+    if guess == number:
+        break
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print("Perfect guess")
+if guess != number:
+    number = str(number)
+    print("Not a correct guess")
